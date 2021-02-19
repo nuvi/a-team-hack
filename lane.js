3647e21e429a2b64bd85eacc6efbd0e826c4117b
lane-c-wagner <lane.c.wagner@gmail.com>
2021-02-19 14:06:10 -0700

    remove lighting changes

--- lane.js
+++ lane.js
@@ -5,7 +5,7 @@ function laneInit() {
     window.GLOBAL_GL.scene.background = texture
 
     const lightColor = 0xfff7ba
-    sunLight = new THREE.PointLight(lightColor, 0);
+    sunLight = new THREE.PointLight(lightColor, 1);
 
     let sun = new THREE.Object3D();
     var loader = new THREE.TextureLoader();
@@ -21,26 +21,11 @@ function laneInit() {
     window.GLOBAL_GL.laneObjects.sunLight = sunLight
     window.GLOBAL_GL.laneObjects.sunFading = false
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0);
+    const ambientLight = new THREE.AmbientLight(0xffffff, .25);
     scene.add(ambientLight);
     window.GLOBAL_GL.laneObjects.ambientLight = ambientLight
 }
 
 function laneRenderer() {
     window.GLOBAL_GL.laneObjects.sun.rotation.y += .01
-
-    const lightChangeFreq = .01
-    if (window.GLOBAL_GL.laneObjects.sunFading) {
-        window.GLOBAL_GL.laneObjects.sunLight.intensity -= lightChangeFreq
-        window.GLOBAL_GL.laneObjects.ambientLight.intensity -= lightChangeFreq / 5
-    } else {
-        window.GLOBAL_GL.laneObjects.sunLight.intensity += lightChangeFreq
-        window.GLOBAL_GL.laneObjects.ambientLight.intensity += lightChangeFreq / 5
-    }
-    if (window.GLOBAL_GL.laneObjects.sunLight.intensity < 0) {
-        window.GLOBAL_GL.laneObjects.sunFading = false
-    }
-    if (window.GLOBAL_GL.laneObjects.sunLight.intensity > 3) {
-        window.GLOBAL_GL.laneObjects.sunFading = true
-    }
 }
