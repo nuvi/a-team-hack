7b8090d90861da5d6b4414948817dc0bd4fc569c
lane-c-wagner <lane.c.wagner@gmail.com>
2021-02-19 12:25:59 -0700

    sun size

--- lane.js
+++ lane.js
@@ -1,21 +1,16 @@
 function laneInit() {
-    // make a sphere and add it to the scene
-    let geometry = new THREE.SphereGeometry(25, 25, 25);
-    let wireframe = new THREE.WireframeGeometry(geometry);
-    let line = new THREE.LineSegments(wireframe);
-    line.material.depthTest = false;
-    line.material.opacity = 0.25;
-    line.material.transparent = true;
-    window.GLOBAL_GL.scene.add(line);
-    window.GLOBAL_GL.laneObjects.line = line
-
     const lightColor = 0xfff7ba
     sunLight = new THREE.PointLight(lightColor, 2, 50);
-    sunLight.add(new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 8), new THREE.MeshBasicMaterial({ color: lightColor })));
+    const sun = new THREE.Mesh(
+        new THREE.SphereGeometry(5, 12, 6),
+        new THREE.MeshBasicMaterial({ color: lightColor })
+    )
+    sunLight.add(sun);
     window.GLOBAL_GL.scene.add(sunLight);
+    window.GLOBAL_GL.laneObjects.sun = sun
 
     // test mesh for the light - delete later
-    geometry = new THREE.SphereGeometry(5, 50, 50);
+    geometry = new THREE.SphereGeometry(3, 50, 50);
     material = new THREE.MeshPhysicalMaterial({ color: 0xcccccc });
     let sphere2 = new THREE.Mesh(geometry, material);
     sphere2.position.x = 15
@@ -23,5 +18,5 @@ function laneInit() {
 }
 
 function laneRenderer() {
-    window.GLOBAL_GL.laneObjects.line.rotation.y += .01
+    window.GLOBAL_GL.laneObjects.sun.rotation.y += .01
 }
